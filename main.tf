51fb767881bf8818632c7c0ab80a9c2ceca745b0
Than Htike Soe <tahs01.ne@gmail.com>
2024-01-01 04:59:58 +0630

    changed the main.tf by tahs

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.26.0"
+    }
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
