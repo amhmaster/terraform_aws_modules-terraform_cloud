f450d75229de9d0668f3db53db48abd98c8e6341
vagrant <vagrant@hellocloud>
2024-04-27 18:47:58 +0000

    Test02

--- version.tf
+++ version.tf
@@ -6,24 +6,24 @@ terraform {
       version = ">= 5.26.0"
     }
   }
-}  
+ 
 
-#   cloud {
-#     workspaces {
-#       name = "terraform_aws_modules-terraform_cloud"
-#     }
-#   }
-# }
-
-# provider "aws" {
-#   region  = var.aws_region
-# }
+  cloud {
+    workspaces {
+      name = "terraform_aws_modules-terraform_cloud"
+    }
+  }
+}
 
 provider "aws" {
-  shared_config_files      = ["/home/vagrant/.aws/config"]
-  shared_credentials_files = ["/home/vagrant/.aws/credentials"]
-  profile                  = "amh-bca-aws-prod"
-  # alias                  = "thanhtikesoe"              
-  region = var.aws_region
+  region  = var.aws_region
 }
+
+# provider "aws" {
+#   shared_config_files      = ["/home/vagrant/.aws/config"]
+#   shared_credentials_files = ["/home/vagrant/.aws/credentials"]
+#   profile                  = "amh-bca-aws-prod"
+#   # alias                  = "thanhtikesoe"              
+#   region = var.aws_region
+# }
 
